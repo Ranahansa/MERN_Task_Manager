286852541c0089d8af817b29c84ea0d5968d5492
Ranahansa <CODSE213F-053@student.nibm.lk>
2023-12-03 08:46:52 +0530

    commit 04

--- server/models/Task.js
+++ server/models/Task.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const taskSchema = new mongoose.Schema({
     name: {
         type: String,
+        trim : true,
         required: true,
+        maxlength: 20,
     },
     completed: {
         type: Boolean,
